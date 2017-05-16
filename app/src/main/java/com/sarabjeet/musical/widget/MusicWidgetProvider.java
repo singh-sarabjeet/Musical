cfb9438620858078be771f4f10f03a340c91bd51
sarabjeet <sjsingh200893@gmail.com>
2017-05-17 01:50:42 +0530

    Redesigned app widget

--- app/src/main/java/com/sarabjeet/musical/widget/MusicWidgetProvider.java
+++ app/src/main/java/com/sarabjeet/musical/widget/MusicWidgetProvider.java
@@ -20,11 +20,11 @@ public class MusicWidgetProvider extends AppWidgetProvider {
 
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-            views.setTextViewText(R.id.widget_song_artist, "Artist");
+           /* views.setTextViewText(R.id.widget_song_artist, "Artist");
             views.setTextViewText(R.id.widget_song_title, "Title");
             views.setImageViewResource(R.id.widget_album_art, R.drawable.fallback_cover);
             views.setImageViewResource(R.id.widget_play_button, R.drawable.ic_play_arrow);
-            views.setImageViewResource(R.id.widget_next_button, R.drawable.ic_skip_next);
+            views.setImageViewResource(R.id.widget_next_button, R.drawable.ic_skip_next);*/
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
